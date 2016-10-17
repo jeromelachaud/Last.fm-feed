975333783c9b7f42c0a70e4dbc216a40310f9483
Jerome Lachaud <jerome.lachaud@gmail.com>
2016-10-17 18:40:06 +0100

    Display User information

--- template/src/components/User.js
+++ template/src/components/User.js
@@ -5,19 +5,41 @@ let User = React.createClass ({
 
   render() {
     const {
-      name,
-      playcount,
-      date
+      user
     } = this.props;
 
+    const {
+      name,
+      playcount,
+      registered,
+      image
+    } = user;
+
+    let imageElement;
+    if (image) {
+      imageElement = (
+        <img src={image['3']['#text']} />
+      );
+    }
+
+    let registeredElement;
+    if (registered) {
+      registeredElement = (
+        <li>{registered['#text']}</li>
+      );
+    }
     return (
       <div
         className="User">
+        <div
+          className="RecentTrack__image">
+          {imageElement}
+        </div>
         <ul
           className="User__info">
           <li>{name}</li>
           <li>{playcount}</li>
-          <li>{date}</li>
+          {registeredElement}
         </ul>
       </div>
     );
