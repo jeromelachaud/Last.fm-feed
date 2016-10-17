87bf28239bb68ef5295f2041796b85f9e4903924
Jerome Lachaud <jerome.lachaud@gmail.com>
2016-10-17 18:39:52 +0100

    Display the list of recent tracks

--- template/src/components/RecentTrack.js
+++ template/src/components/RecentTrack.js
@@ -4,19 +4,37 @@ import './RecentTrack.css';
 let RecentTrack = React.createClass ({
 
   render() {
-    const {name} = this.props.name;
-    const {url} = this.props.url;
+    const {
+      recentTrack
+    } = this.props;
+
+    const {
+      artist,
+      album,
+      date,
+      image
+    } = recentTrack;
+
+    let dateElement;
+    if (date) {
+      dateElement = (
+        <li className="RecentTrack__date">{date['#text']}</li>
+      );
+    }
 
     return (
       <ul
         className="RecentTrack">
         <li
           className="RecentTrack_item">
-          <a href="">
-            <img src={url} />
+          <a href={recentTrack.url}>
+            <img src={image['3']['#text']} />
             <ul
               className="RecentTrack__info">
-              <li>{name}</li>
+              <li><strong>{artist['#text']}</strong></li>
+              <li>{recentTrack.name}</li>
+              <li><em>{album['#text']}</em></li>
+              {dateElement}
             </ul>
           </a>
         </li>
