536dafedd291178eb967933522d5de4c0e1e42fb
Jerome Lachaud <jerome.lachaud@gmail.com>
2016-10-21 18:44:00 +0100

    Add constants

---
+++ template/src/constants/constants.js
@@ -0,0 +1,10 @@
+const apiConstants =  {
+  baseURL : 'http://ws.audioscrobbler.com/2.0/',
+  apiKey : 'ce36645ab9a1ec3fc807f3ef8ef729b7',
+  userName : 'PatBateman75',
+  getMethod : 'get'
+};
+
+export {
+  apiConstants
+};
