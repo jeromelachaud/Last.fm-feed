25d25f2d1cfbb6f742c8e55997e451c444f299db
Jerome Lachaud <jerome.lachaud@gmail.com>
2016-11-25 13:52:29 +0000

    Toggle on/off the user info

--- template/src/components/Menu.js
+++ template/src/components/Menu.js
@@ -7,6 +7,19 @@ import User from './User';
 
 let Menu = React.createClass ({
 
+  getInitialState() {
+    return { showResults: false };
+  },
+
+  onClick(e) {
+    e.preventDefault(e);
+    if (this.state.showResults === false) {
+      this.setState({showResults : true});
+    } else {
+      this.setState({showResults : false});
+    }
+  },
+
   render() {
 
     const {
@@ -32,18 +45,13 @@ let Menu = React.createClass ({
             <a href="#0" className="Menu__item">Charts</a>
           </li>
           <li>
-            <a href="/" className="Menu__item" onClick={this.handleClick}>User infos</a>
+            <a href="#0" className="Menu__item" onClick={this.onClick}>User infos</a>
           </li>
         </ul>
-        {userElement}
+        {this.state.showResults ? userElement : null}
       </div>
     );
-  },
-
-  handleClick: function() {
-    // To-DO: toggle the display of the user card
   }
-
 });
 
 export default Menu;
