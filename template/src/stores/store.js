f575553df91fbad290d15d5de62d9c330bd11a1b
Jerome Lachaud <jerome.lachaud@gmail.com>
2017-02-24 16:43:08 +0000

    Regroup actions handlers

--- template/src/stores/store.js
+++ template/src/stores/store.js
@@ -34,70 +34,41 @@ const store = new Store({
 
 export default store;
 
-export function handleUserRetrieved(state, action) {
-  if (!action.user) {
-    throw new Error('No user');
+var handlers = {
+
+  userRetrieved: function(state, action) {
+    if (!action.user) {
+      throw new Error('No user');
+    }
+
+    state.user = action.user;
+    store.emitChange();
+    return state;
+  },
+
+  recentTracksRetreived: function(state, action) {
+    if (!action.recentTracks) {
+      throw new Error('No recent tracks');
+    }
+
+    state.recentTracks = action.recentTracks;
+    store.emitChange();
+    return state;
+  },
+
+  topArtistsRetreived: function(state, action) {
+    if (!action.topArtists) {
+      throw new Error('No top artists');
+    }
+
+    state.topArtists = action.topArtists;
+    store.emitChange();
+    return state;
   }
-
-  state.user = action.user;
-  store.emitChange();
-  return state;
-}
-
-export function recentTracksRetreived(state, action) {
-  if (!action.recentTracks) {
-    throw new Error('No recent tracks');
-  }
-
-  state.recentTracks = action.recentTracks;
-  store.emitChange();
-  return state;
-}
-
-
-export function topArtistsRetreived(state, action) {
-  if (!action.topArtists) {
-    throw new Error('No top artists');
-  }
-
-  state.topArtists = action.topArtists;
-  store.emitChange();
-  return state;
-}
-
-// var handlers = {
-//   userRetrieved: function(state, action) {
-//     if (!action.user) {
-//       throw new Error('No user');
-//     }
-//
-//     state.user = action.user;
-//     store.emitChange();
-//     return state;
-//   },
-//
-//   recentTracksRetreived: function(state, action) {
-//
-//   }
-// }
+};
 
 dispatcher.register((action) => {
 
-  // handles[action.type](state, action);
+  handlers[action.type](state, action);
 
-  switch (action.type) {
-    case 'userRetrieved':
-      handleUserRetrieved(state, action);
-      break;
-
-    case 'recentTracksRetreived':
-      recentTracksRetreived(state, action);
-      break;
-
-    case 'topArtistsRetreived':
-      topArtistsRetreived(state, action);
-      break;
-
-    default:
-  }
 });
