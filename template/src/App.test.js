68c4e1c789495ce09ada9ab4087be0b97d8c3180
Jerome Lachaud <jerome.lachaud@gmail.com>
2017-02-24 16:07:39 +0000

    Create app test file template

---
+++ template/src/App.test.js
@@ -0,0 +1,7 @@
+
+// it('should add the user to the state', function() {
+//   var newState = handleUserRetrieved({}, { user: whatever });
+//
+//   expect(newState.user)
+//     .toBe(whatever)
+// })
