dac3bf90b286690c7f2c2b9e5283d82173e23cbb
Jerome Lachaud <jerome.lachaud@gmail.com>
2017-03-13 22:57:12 +0000

    Add Menu snapshot

--- src/components/Menu.test.js
+++ src/components/Menu.test.js
@@ -2,11 +2,12 @@ import React from 'React';
 import {
   shallow
 } from 'enzyme';
+import toJson from 'enzyme-to-json';
 import Menu from './Menu';
 
 describe('Menu componemts', () => {
   it('Menu component should render as expected', () => {
-    const component = shallow(<Menu />);
-    console.log(component);
+    const menuComponent = shallow(<Menu />);
+    expect(toJson(menuComponent)).toMatchSnapshot();
   });
 });
