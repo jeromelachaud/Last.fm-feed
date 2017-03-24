b1252f5f7509ff29d20af5720b0b32e3057c20de
Jerome Lachaud <jerome.lachaud@gmail.com>
2017-03-24 14:36:31 +0000

    Testing that the actions handlers throw error when not provided with expect parameters

--- src/stores/lastfm-store.test.js
+++ src/stores/lastfm-store.test.js
@@ -4,27 +4,51 @@ import {
 
 describe('LastFm activities actions handlers', () => {
 
-  it('should add the user to the state', () => {
-    var actionType = handlers.userRetrieved;
-    var newState = actionType({}, { user: 'user' });
+  describe('userRetrieved action handler', () => {
+    const actionType = handlers.userRetrieved;
 
-    expect(newState.user)
-      .toBe('user');
+    it('should add the user to the state', () => {
+      const newState = actionType({}, { user: 'user' });
+      expect(newState.user)
+        .toBe('user');
+    });
+
+    it('should throw an error if user does not exists', () => {
+      expect(() => {
+        handlers.userRetrieved({}, {});
+      }).toThrowError('No user');
+    });
   });
 
-  it('should add the recentTracks to the state', () => {
-    var actionType = handlers.recentTracksRetreived;
-    var newState = actionType({}, { recentTracks: 'recentTracks' });
+  describe('recentTracksRetreived action handler', () => {
+    const actionType = handlers.recentTracksRetreived;
+    
+    it('should add the recentTracks to the state', () => {
+      const newState = actionType({}, { recentTracks: 'recentTracks' });
+      expect(newState.recentTracks)
+        .toBe('recentTracks');
+    });
 
-    expect(newState.recentTracks)
-      .toBe('recentTracks');
+    it('should throw an error if recentTracks does not exists', () => {
+      expect(() => {
+        handlers.recentTracksRetreived({}, {});
+      }).toThrowError('No recent tracks');
+    });
   });
 
-  it('should add the topArtists to the state', () => {
-    var actionType = handlers.topArtistsRetreived;
-    var newState = actionType({}, { topArtists: 'topArtists' });
-
-    expect(newState.topArtists)
-      .toBe('topArtists');
+  describe('topArtistsRetreived action handler', () => {
+    const actionType = handlers.topArtistsRetreived;
+    
+    it('should add the topArtists to the state', () => {
+      const newState = actionType({}, { topArtists: 'topArtists' });
+      expect(newState.topArtists)
+        .toBe('topArtists');
+    });
+    
+    it('should throw an error if recentTracks does not exists', () => {
+      expect(() => {
+        handlers.topArtistsRetreived({}, {});
+      }).toThrowError('No top artists');
+    });
   });
 });
