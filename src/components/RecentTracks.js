ba3b920b61adaeda0a3a219f6f6c237b3acf770c
Jerome Lachaud <jerome.lachaud@gmail.com>
2017-03-24 16:51:17 +0000

    Check if a track is playing

--- src/components/RecentTracks.js
+++ src/components/RecentTracks.js
@@ -2,14 +2,20 @@ import React from 'react';
 import './RecentTracks.css';
 import RecentTrack from './RecentTrack.js';
 import _map from 'lodash/map';
+import _isEqual from 'lodash/isEqual';
 import {
   fetchRecentTracks
 } from '../actions/action-creators';
 
 let RecentTracks = React.createClass ({
 
+  shouldComponentUpdate(nextProps){
+    return !_isEqual(nextProps.recentTracks,this.props.recentTracks);
+  },
+
   componentWillMount() {
     fetchRecentTracks();
+    // this.updateInterval = setInterval(() => fetchRecentTracks(2), 3000); 
   },
 
   render() {
