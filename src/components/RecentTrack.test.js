5adbede91336551cd067a6b19f7980388c830edd
Jerome Lachaud <jerome.lachaud@gmail.com>
2017-03-24 15:33:04 +0000

    Add test for RecentTrack component

---
+++ src/components/RecentTrack.test.js
@@ -0,0 +1,26 @@
+import React from 'React';
+import {
+  shallow
+} from 'enzyme';
+import toJson from 'enzyme-to-json';
+import RecentTrack from './RecentTrack';
+
+const recentTrack = {
+  'name': 'a name',
+  'image': [
+    {'#text' : 'album'},
+    {'#text' : 'album'},
+    {'#text' : 'album'},
+    {'#text' : 'album'}
+  ],
+  'artist': ['#text'],
+  'album': ['#text']
+};
+
+describe('<RecentTrack /> componemts', () => {
+  const wrapper = shallow(<RecentTrack recentTrack={recentTrack}/>);
+
+  it('should render as expected', () => {
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+});
