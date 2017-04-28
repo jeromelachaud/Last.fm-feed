0a7b4fe26c5721bf93ee7bafa4b0505c7a120a3c
Jerome Lachaud <jerome.lachaud@gmail.com>
2017-04-28 16:59:04 +0100

    Update App component styles

--- src/App.js
+++ src/App.js
@@ -6,9 +6,6 @@ import Topbar from './components/Topbar';
 import styled from 'styled-components';
 
 const StyledApp = styled.div`
-  width: 100%;
-  margin: 0 auto;
-  position: relative;
   font-size: 2rem;
 `;
 
